2f1ab74185d5e3a5747942e63b5d23c79e021a03
Kalila L <kasenvr@gmail.com>
2020-08-21 20:38:19 -0400

    Fix refreshing of scripts...

--- scripts/system/slider/public/slider-client.js
+++ scripts/system/slider/public/slider-client.js
@@ -234,6 +234,7 @@
     };
     
     this.unload = function(entityID) {
+        Entities.webEventReceived.disconnect(onWebAppEventReceived);
         Messages.messageReceived.disconnect(onMessageReceived);
         Messages.unsubscribe(presentationChannel);
     };
